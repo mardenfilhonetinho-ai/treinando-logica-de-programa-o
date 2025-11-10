3ba45efde12303e8936c55ed8ad123dd27d96f84
mardenfilhonetinho-ai <mardenfilho.netinho@gmail.com>
2025-11-10 09:28:27 -0400

    botão reiniciar funcionando

--- sorteador-numeros/app.js
+++ sorteador-numeros/app.js
@@ -27,9 +27,20 @@ function obterNumeroAleatorio(min, max) {
 
 function altararEstadoBotao() {
   let botao = document.getElementById('btn-reiniciar');
-  if (botao) {
-    botao.disabled = false;
+  if (botao.classList.contains('container__botao-desabilitado')) {
+    botao.classList.remove('container__botao-desabilitado');
+    botao.classList.remove('container__botao');
   } else {
-    botao.disabled = true;
+    botao.classList.remove('container__botao');
+    botao.classList.add('container__botao-desabilitado');
+    
   }
+}
+
+function reiniciar(){
+  document.getElementById('quantidade').value = '';
+  document.getElementById('de').value = '';
+  document.getElementById('ate').value = '';
+  document.getElementById('resultado').innerHTML = ' <label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>';
+  altararEstadoBotao();
 }
